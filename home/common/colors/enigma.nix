b5130bbdbc3c2a09a1212575595ae9315e0a62d8
froehlichA <froehlich.alexander@protonmail.com>
2020-01-01 15:30:00 +0100

    change enigma color and background

--- home/common/colors/enigma.nix
+++ home/common/colors/enigma.nix
@@ -2,23 +2,23 @@
 
 {
     style.colors = {
-		background = "#010101";
-		foreground = "#f8d8f8";
-		color0 = "#1d2b53";
-		color8 = "#1d2b53";
-		color1 = "#ff004d";
-		color9 = "#ff004d";
-		color2 = "#83769c";
-		color10 = "#83769c";
-		color3 = "#ffccaa";
-		color11 = "#ffccaa";
-		color4 = "#29adff";
-		color12 = "#29adff";	
-		color5 = "#7e2553";
-		color13 = "#7e2553";
-		color6 = "#ff77a8";
-		color14 = "#ff77a8";
-		color7 = "#fff1e8";
-		color15 = "#fff1e8";
+		background = "#292827";
+		foreground = "#e4e9ec";
+		color0 = "#495c65";
+		color8 = "#485c64";
+		color1 = "#f7cac9";
+		color9 = "#d9b1b1";
+		color2 = "#9fa19c";
+		color10 = "#c1beb7";
+		color3 = "#e0cbd4";
+		color11 = "#af9ea5";
+		color4 = "#91a8cf";
+		color12 = "#677793";	
+		color5 = "#8b85a5";
+		color13 = "#656178";
+		color6 = "#c1cfcf";
+		color14 = "#858f8f";
+		color7 = "#c9d3de";
+		color15 = "#e4e9ec";
 	};
 }
