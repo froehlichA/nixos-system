afaaa66e017a2baa297c1d3a3c006bb431b6230d
froehlichA <froehlich.alexander@protonmail.com>
2020-11-10 11:57:47 +0100

    add teams

--- home/work.nix
+++ home/work.nix
@@ -2,6 +2,7 @@
 {
     home.packages = with pkgs; [
         slack # communication
+        teams # also communication
         flameshot # screenshot tool
         peek # screen recorder
         vlc # video player
